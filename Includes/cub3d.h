cf10b0d3d9205a9dfb3b0f1223488d35a1e5241f
Anahit Apresyan <aapresya@c2r12s5.42yerevan.am>
2022-09-11 17:30:47 +0400

    Merging

--- Includes/cub3d.h
+++ Includes/cub3d.h
@@ -1,55 +1,104 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <mlx.h>
-#include "libft/libft.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aapresya <aapresya@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/10 14:20:46 by aapresya          #+#    #+#             */
+/*   Updated: 2022/09/11 16:38:02 by aapresya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <math.h>
+# include <mlx.h>
+# include "libft/libft.h"
+# define BUFFER_SIZE 1
 
 typedef struct s_texture{
-    char    *id;
-    char    *file_name;
-}   t_texture;
+	char	*id;
+	char	*file_name;
+}	t_texture;
 
 typedef struct s_rgb{
-    int r;
-    int g;
-    int b;
-}   t_rgb;
+	int	r;
+	int	g;
+	int	b;
+}	t_rgb;
 
 typedef struct s_ceil_floor{
-    char    id;
-    t_rgb   rgb;
-}   t_ceil_floor;
+	char	id;
+	t_rgb	rgb;
+}	t_ceil_floor;
+
+typedef struct s_map
+{
+	int		width;
+	int		height;
+	char	**mat;
+}	t_map;
 
 typedef struct s_file {
-    t_texture       texture[4];
-    t_ceil_floor    cf[2];
-    int             **map;
-}   t_file;
+	t_texture		texture[4];
+	t_ceil_floor	cf[2];
+}	t_file;
 
 typedef struct s_xy{
-    double x;
-    double y;
-} t_xy;
+	double	x;
+	double	y;
+}	t_xy;
 
 typedef struct s_world{
-    t_xy    pos;
-    t_xy    raydir;
-    t_xy    sidedist;
-    t_xy    deltadist;
-    t_xy    step;
-    int     side;
-    int     hit;
-    double  perpwalldist;
-} t_world;
+	t_xy	pos;
+	t_xy	raydir;
+	t_xy	sidedist;
+	t_xy	deltadist;
+	t_xy	step;
+	int		side;
+	int		hit;
+	double	perpwalldist;
+}	t_world;
 
 typedef struct s_mlx{
-    void    *mlx;
-    void    *mlx_win;
-}   t_mlx;
+	void	*mlx;
+	void	*mlx_win;
+}	t_mlx;
 
-int parsing(int argc, char **argv);
+typedef struct s_data
+{
+	t_map	map;
+}	t_data;
+
+extern t_data	g_data;
+
+int		parsing(int argc, char **argv);
+char	*the_line(char *tmp);
+char	*leftover(char *tmp);
+char	*ft_strjoin_gln(char *s1, char *s2);
+size_t	ft_strlen_gln(char *str);
+int		get_next_line(int fd, char **line);
+int		read_map(char *filename);
+int		find_map(char *filename);
+void	get_height_width(int fd);
+int		ft_error(char *err);
+void	print_mat(char **mat, int len);
+int		iterate_spaces(int y, int *x);
+int		on_edge(int y, int x);
+int		surrounded_by_space(int y, int x, char c);
+int		check_row(int y);
+int		check_walls(void);
+int		check_chars(void);
+int		valid_char(char c);
+void	fill_map(int i, int *j, char *line);
+
+#endif
---
+++ Makefile
@@ -0,0 +1,50 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: aapresya <aapresya@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2022/06/28 21:36:19 by aapresya          #+#    #+#              #
+#    Updated: 2022/09/11 16:51:22 by aapresya         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME	= cub3d
+
+MLX		= -Lmlx -lmlx -framework OpenGL -framework AppKit -lm
+SRCS	= $(wildcard srcs/*.c)
+OBJS	= ${SRCS:.c=.o}
+CC		= gcc
+CFLAGS	= #-O3 -Wall -Wextra -Werror
+RM		= rm -rf
+
+all: $(NAME)
+
+${NAME}: ${OBJS}
+	@echo [cub3d] Compiling mlx...
+	@$(MAKE) -C ./mlx
+	@echo [cub3d] Compiling libft...
+	@$(MAKE) -C Includes/libft
+	@mv mlx/libmlx.dylib .
+	@echo [cub3d] Compiling cub3d...
+	@${CC} ${OBJS} ${CFLAGS} ${MLX} Includes/libft/libft.a libmlx.dylib -o ${NAME}
+	@echo [cub3d] cub3d successfully compiled. Run ./cub3d \*.cub to read from a map. 
+
+clean:
+	@$(MAKE) -C mlx clean
+	@$(MAKE) -C Includes/libft fclean
+	@${RM} ${OBJS}
+
+fclean: clean
+	@${RM} ${NAME}
+	@${RM} libmlx.dylib
+
+norme:
+	@norminette -R CheckForbiddenSourceHeader *.c *.h libft/*.c libft/*.h
+	
+bonus: all
+
+re: fclean all
+
+.PHONY: clean fclean re all bonus
